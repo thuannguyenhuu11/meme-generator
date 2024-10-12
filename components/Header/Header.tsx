c191b443579a3cb849306b6a82af721901842fd1
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-10-13 03:32:45 +0700

    feat: add searchbar

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { redirect } from 'next/navigation';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -44,12 +45,20 @@ const Header = () => {
         </SheetContent>
       </Sheet>
       <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-        <form className="ml-auto flex-1 sm:flex-initial">
+        <form
+          action={async formData => {
+            'use server';
+            const search = formData.get('search');
+            redirect(`/search?q=${search}`);
+          }}
+          className="ml-auto flex-1 sm:flex-initial"
+        >
           <div className="relative">
             <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
             <Input
+              name="search"
               type="search"
-              placeholder="Search products..."
+              placeholder="Search memes..."
               className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
             />
           </div>
